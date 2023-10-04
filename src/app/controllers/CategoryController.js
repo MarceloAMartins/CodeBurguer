3919ababeb4c34fa1b3c643359e173ee99d12f74
Marcelo <marcelo.antonio1977@hotmail.com>
2023-10-03 21:40:03 -0300

    backend finalizado

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,53 +1,94 @@
-import * as Yup from 'yup'
-import Category from '../models/Category'
-import User from './../models/User';
+import * as Yup from "yup";
+import Category from "../models/Category";
+import User from "./../models/User";
 
-class CategoryController{
-    async store(request, response) {
-        try {
-        const schema =  Yup.object().shape({
-            name: Yup.string().required(),           
-        })
+class CategoryController {
+  async store(request, response) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+      });
 
       try {
-        await schema.validateSync(request.body, { abortEarly: false})
-      }catch (err) {
-            return response.status(400).json({ error: err.errors })
-        }  
+        await schema.validateSync(request.body, { abortEarly: false });
+      } catch (err) {
+        return response.status(400).json({ error: err.errors });
+      }
 
-        const { admin: isAdmin } = await User.findByPk(request.userId)
+      const { admin: isAdmin } = await User.findByPk(request.userId);
 
-        if(!isAdmin){
-            return response.status(401).json()
-        }
-        
+      if (!isAdmin) {
+        return response.status(401).json();
+      }
 
-        const { name } = request.body
+      const { name } = request.body;
 
-        const categoryExists = await Category.findOne({
-            where: {name},
-       })
+      const { filename: path } = request.file;
 
-       if(categoryExists){
-       return response.status(400).json({ error : 'category already exists'})
-       }
+      const categoryExists = await Category.findOne({
+        where: { name },
+      });
 
-        const {id} = await Category.create({name})
+      if (categoryExists) {
+        return response.status(400).json({ error: "category already exists" });
+      }
 
-        return response.json({id, name})
+      const { id } = await Category.create({ name, path });
+
+      return response.json({ id, name });
     } catch (err) {
-        console.log(err)
+      console.log(err);
     }
+  }
+
+  async index(request, response) {
+    const category = await Category.findAll();
+
+    return response.json(category);
+  }
+
+  async update(request, response) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+      });
+
+      try {
+        await schema.validateSync(request.body, { abortEarly: false });
+      } catch (err) {
+        return response.status(400).json({ error: err.errors });
+      }
+
+      const { admin: isAdmin } = await User.findByPk(request.userId);
+
+      if (!isAdmin) {
+        return response.status(401).json();
+      }
+
+      const { name } = request.body;
+
+      const { id } = request.params;
+
+      const category = await Category.findByPk(id);
+
+      if (!category) {
+        return response
+          .status(401)
+          .json({ error: "Make sure your category id is correct" });
+      }
+
+      let path;
+      if (request.file) {
+        path = request.file.filename;
+      }
+
+      await Category.update({ name, path }, {where: {id}});
+
+      return response.status(200).json();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
-    
 
-async index(request,response){
-    const category= await Category.findAll()
-   
-    return response.json(category)
-}
-}
-
-
-
-export default new CategoryController ()
+export default new CategoryController();
